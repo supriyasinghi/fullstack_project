58593b938ba19d04d058ca13c92ebf2153313186
madisen-hallberg <madisen3@pdx.edu>
2021-02-23 17:11:16 -0800

    HTML
    used Pug to display the event names from SQL table in html table on the web. Next I am going to find out how to create a searchable table with search field.

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
 var mysql = require('mysql');
+const axios = require("axios");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
 
-var con = mysql.createConnection({
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
+
+const con = mysql.createConnection({
     host: "localhost",
     user: "eventplanner",
     password: "eventplanner",
     database: "events"
 });
 
-con.connect(function(err){
-    if (err) throw err;
-    con.query("SELECT * FROM events", function(err, result, fields){
-        if(err) throw err;
-        console.log(result);
-    })
+
+
+app.get('/', async(req, res) => {
+    
+    con.connect(function(err){
+        if (err) throw err;
+        con.query("SELECT * FROM events", function(err, result, fields){
+            if(err) throw err;
+            const data = result;
+            console.log(data);
+            res.render("main", {events: data});
+        })
+    });
+})
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
 });
