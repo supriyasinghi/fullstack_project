cc999942400a0b01480691add81b7686382172f8
madisen-hallberg <madisen3@pdx.edu>
2021-02-26 11:33:32 -0800

    delete main.js

--- app/public/main.js
+++
@@ -1,9 +0,0 @@
-const search = document.getElementById("search");
-search.addEventListener('keyup', function(e,req,res){
-    if(e.key==='Enter'){
-        var search_url =  '/search?query=' + e.value;
-        res.redirect(search_url, 301)
-        window.location.href = search_url
-    }
-})
-console.log("hello world");
