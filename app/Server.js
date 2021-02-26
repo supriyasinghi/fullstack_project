3d4246311796c9352fd64ed8cf2d496dcc74f15f
madisen-hallberg <madisen3@pdx.edu>
2021-02-26 11:30:05 -0800

    search working

--- app/Server.js
+++ app/Server.js
@@ -8,7 +8,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 //supriya
-const parser = require("body-parser");
+
+const parser = require('body-parser');
+const {body,validationResult} = require('express-validator');
+enctype="application/x-www-form-urlencoded";
 
 app.set('main', __dirname + '/components');
 app.set('view engine', 'pug');
@@ -36,12 +39,12 @@ app.get('/', async(req, res) => {
 })
 
 app.get('/search*', function(req,res){
-    var search_url=req.params //supriya
-    console.log(`searchURL - ${search_url}`)     //supriya
-    console.log(req.query.key)  //supriya
+    let searchParam = req.query.query;
+    console.log(searchParam)  //supriya
     pool.getConnection(function(err, connection){
         if (err) throw err;
-        connection.query('SELECT * FROM events where city like "%'+req.query.key+'%"',
+        console.log('SELECT * FROM events where city like "%'+searchParam+'%"');
+        connection.query('SELECT * FROM events where city like "%'+searchParam+'%"',
         function(err, result){
             if(err) throw err;
             const data = result;
