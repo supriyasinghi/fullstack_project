49db70dcb7c10e7ebf54b377462296f4af2c4654
madisen-hallberg <madisen3@pdx.edu>
2021-02-26 11:32:35 -0800

    changes from trial

--- app/Server.js
+++ app/Server.js
@@ -7,11 +7,16 @@ const axios = require("axios");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
-const parser = require("body-parser");      //supriya
+//supriya
+
+const parser = require('body-parser');
+const {body,validationResult} = require('express-validator');
+enctype="application/x-www-form-urlencoded";
 
 app.set('main', __dirname + '/components');
 app.set('view engine', 'pug');
-app.use(parser.json());                     //supriya
+//supriya
+app.use(parser.json());
 
 const pool = mysql.createPool({
     connectionLimit : 10,
@@ -33,13 +38,13 @@ app.get('/', async(req, res) => {
     });
 })
 
-app.get('/search', function(req,res){
-    var search_url=req.params.query //supriya
-    console.log(`searchURL - ${search_url}`)     //supriya
-    console.log(req.query.key)  //supriya
+app.get('/search*', function(req,res){
+    let searchParam = req.query.query;
+    console.log(searchParam)  //supriya
     pool.getConnection(function(err, connection){
         if (err) throw err;
-        connection.query('SELECT * FROM events where country like "%'+req.query.key+'%"',
+        console.log('SELECT * FROM events where city like "%'+searchParam+'%"');
+        connection.query('SELECT * FROM events where city like "%'+searchParam+'%"',
         function(err, result){
             if(err) throw err;
             const data = result;
